93df9e6b277e2a5af0995cd6f97c3d5ea9dccdf0
adi-devv <aaditsingal7859@gmail.com>
2024-08-07 17:42:46 +0530

    init

--- ui.py
+++ ui.py
@@ -1,22 +1,25 @@
 from tkinter import *
 from tkinter.messagebox import *
 
+
 def quit_app():
     if askyesno('Verify', 'Do you really want to quit?'):
         showwarning('Yes', 'Quit not yet implemented')
     else:
         showinfo('No', 'Quit has been cancelled')
 
+
 def show_spam():
     showerror('Spam', 'Sorry, no Spam allowed!')
+
 
 root = Tk()
 
 # Add Checkbuttons
-for i in range(3): Checkbutton(root, text=f'Item {i+1}').pack()
+for i in range(3): Checkbutton(root, text=f'Item {i + 1}').pack()
 
 # Add Radiobuttons
-for i in range(2): Radiobutton(root, text=f'Subject {i+1}', value=i+1).pack()
+for i in range(2): Radiobutton(root, text=f'Subject {i + 1}', value=i + 1).pack()
 
 # Add Buttons
 Button(root, text='Quit', command=quit_app).pack(fill=X)
